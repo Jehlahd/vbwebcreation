adc141e4f4618a0acb8c9a2e2229ed094a6f2243
Jehlahd <vincent.boudewyn@gmail.com>
2015-10-12 19:57:43 +0200

    Project Creation
    Creation du projet

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vbwebcreationApp
+ * @description
+ * # vbwebcreationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vbwebcreationApp', [
+    'ngAnimate'
+  ]);
