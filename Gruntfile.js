27162a123a2ab28c8e646e9b1ad3648553aad0ad
Jehlahd <vincent.boudewyn@gmail.com>
2015-10-20 19:07:15 +0200

    test4

--- Gruntfile.js
+++ Gruntfile.js
@@ -462,16 +462,5 @@ module.exports = function (grunt) {
     'build'
   ]);
   
-  grunt.initConfig({
-  bower: {
-    install: {
-       //just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
-    }
-  }
-});
-
-grunt.registerTask('heroku:production', [
-    'build'
-]);
 };
 
