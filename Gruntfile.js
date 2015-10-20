4874fd1d0fd7d0462e7f9515ca3c7b738d267058
Jehlahd <vincent.boudewyn@gmail.com>
2015-10-20 19:18:10 +0200

    test6

--- Gruntfile.js
+++ Gruntfile.js
@@ -456,6 +456,14 @@ module.exports = function (grunt) {
     'htmlmin'
   ]);
 
+  grunt.initConfig({
+  bower: {
+    install: {
+       //just run 'grunt bower:install' and you'll see files from your Bower packages in lib directory
+    }
+  }
+});
+  
   grunt.registerTask('default', [
     'newer:jshint',
     'test',
